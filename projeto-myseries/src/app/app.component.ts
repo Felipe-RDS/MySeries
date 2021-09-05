3bbff67d85314fa974d30870fba139c966c43812
Felipe Rodrigues <Felipetpr@hotmail.com>
2021-09-05 17:07:19 -0300

    Componentes separados

--- projeto-myseries/src/app/app.component.ts
+++ projeto-myseries/src/app/app.component.ts
@@ -10,4 +10,76 @@ declare var $: any;
 })
 export class AppComponent {
   title = 'projeto-myseries';
+
+ngAfterViewInit() {
+
+  // Mobile Navigation
+  if ($('.nav-menu').length) {
+    var $mobile_nav = $('.nav-menu').clone().prop({
+      class: 'mobile-nav d-lg-none'
+    });
+    $('body').append($mobile_nav);
+    $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
+    $('body').append('<div class="mobile-nav-overly"></div>');
+
+    $(document).on('click', '.mobile-nav-toggle', function(e: any) {
+      $('body').toggleClass('mobile-nav-active');
+      $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+      $('.mobile-nav-overly').toggle();
+    });
+
+    $(document).on('click', '.mobile-nav .drop-down > a', function(e: any, item: any) {
+      e.preventDefault();
+      $(item).next().slideToggle(300);
+      $(item).parent().toggleClass('active');
+    });
+
+    $(document).click(function(e: any) {
+      var container = $(".mobile-nav, .mobile-nav-toggle");
+      if (!container.is(e.target) && container.has(e.target).length === 0) {
+        if ($('body').hasClass('mobile-nav-active')) {
+          $('body').removeClass('mobile-nav-active');
+          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+          $('.mobile-nav-overly').fadeOut();
+        }
+      }
+    });
+  } else if ($(".mobile-nav, .mobile-nav-toggle").length) {
+    $(".mobile-nav, .mobile-nav-toggle").hide();
+  }
+
+  // Clients carousel (uses the Owl Carousel library)
+  $(".clients-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    responsive: {
+      0: {
+        items: 2
+      },
+      768: {
+        items: 4
+      },
+      900: {
+        items: 6
+      }
+    }
+  });
+
+  // Testimonials carousel (uses the Owl Carousel library)
+  $(".testimonials-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    items: 1
+  });
+
+  // Portfolio details carousel
+  $(".portfolio-details-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    items: 1
+  });
+}
 }
--- projeto-myseries/src/app/app.module.ts
+++ projeto-myseries/src/app/app.module.ts
@@ -4,11 +4,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './about/about.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { FeaturesComponent } from './features/features.component';
+import { ServicosComponent } from './servicos/servicos.component';
+import { CtaComponent } from './cta/cta.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { CountsComponent } from './counts/counts.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { TeamComponent } from './team/team.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HeroComponent,
+    AboutComponent,
+    ClientesComponent,
+    FeaturesComponent,
+    ServicosComponent,
+    CtaComponent,
+    PortfolioComponent,
+    CountsComponent,
+    TestimonialsComponent,
+    TeamComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
